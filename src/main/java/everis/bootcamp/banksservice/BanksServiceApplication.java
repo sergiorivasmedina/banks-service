0fe0f6b471c7982415de72ad2d0a99330d00f101
Sergio Andre Rivas Medina <srivasme@everis.com>
2020-07-23 11:32:48 -0500

    :tada: Initial commit

---
+++ src/main/java/everis/bootcamp/banksservice/BanksServiceApplication.java
@@ -0,0 +1,13 @@
+package everis.bootcamp.banksservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/everis/bootcamp/banksservice/BanksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package everis.bootcamp.banksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
